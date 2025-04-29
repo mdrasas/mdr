ef553e2001b1445054797cb5c7b5313645cbe54d
mdrasas <minidemandasremuneradas@gmail.com>
2025-04-29 14:30:53 -0300

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'hca-beige': '#f8eab9',
+        'hca-beige-light': '#f7efd6',
+        'hca-brown': '#A67C52',
+        'hca-brown-dark': '#4A4238',
+        'hca-tan': '#D4C3A3',
+      },
+      backgroundImage: {
+        'hero-desktop': "url('assets/img/background.webp')",
+      }
+    }
+  }
+}
